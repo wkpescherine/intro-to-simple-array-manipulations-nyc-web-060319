eea588359752edafb7c925d8bada473c4d856853
William Pescherine <wkpescherine@gmail.com>
2019-05-26 02:50:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,9 +39,9 @@ def using_flatten(array)
 end 
 
 def using_delete(array, string)
-  array.delete[string]
+  array.delete(string)
 end
 
 def using_delete_at(array, value)
-  array.delete[value]
+  array.delete(value)
 end
