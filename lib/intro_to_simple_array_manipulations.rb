9b820d671b7e065b89fa5ba29adb522f7a32a9e1
William Pescherine <wkpescherine@gmail.com>
2019-05-26 02:31:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,3 +9,15 @@ end
 def using_pop(array)
   array.pop
 end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
