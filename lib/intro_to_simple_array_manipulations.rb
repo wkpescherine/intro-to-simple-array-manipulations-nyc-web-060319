d0ac8db684c6540bd7bdb28984cba5c48832f835
William Pescherine <wkpescherine@gmail.com>
2019-05-26 02:46:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,8 +30,8 @@ def using_insert(array, element)
   array.insert(4,element)
 end
 
-def using_uniq(array, element)
-  array.uniq(element)
+def using_uniq(array)
+  array.uniq
 end
 
 def using_flatten(array)
